9ff3b07e173e234c462d249c8ff492e2b7f48d61
TqnvirrDev <77493205+TqnvirrDev@users.noreply.github.com>
2021-01-15 08:18:03 -0500

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,17 @@
+module.exports = {
+  emojis: {
+    off: ':x:',
+    error: ':warning:',
+    queue: ':bar_chart:',
+    music: ':musical_note:',
+    success: ':white_check_mark:',
+  },
+
+  discord: {
+    token: 'Nzc2MjQxNDI1NDIyNjE0NTM5.X6yBKQ.B0yrxvuov_V2l0JsU0NvsLCTcWc',
+    prefix: '!!',
+    activity: 'McDonalds V1',
+  },
+
+  filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
